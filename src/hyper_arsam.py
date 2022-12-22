05046ee0da064c16b095ac5dcc4ab1f027fa5889
A A <arsama@ethz.ch>
2022-12-22 01:25:08 +0100

    made substantial imrovements to accuracy of universal graph representation

--- src/hyper_arsam.py
+++ src/hyper_arsam.py
@@ -12,7 +12,7 @@ class HyperParameter:
     SEED = 3
     
     
-    # Some paths
+    # Paths to data
     PATH_TO_DATA = '../data/'
     
     PATH_TO_UBERMOVEMENT = PATH_TO_DATA + 'UberMovement/'
@@ -20,6 +20,12 @@ class HyperParameter:
     PATH_TO_UBERMOVEMENT_TRAIN = PATH_TO_UBERMOVEMENT + 'training/'
     PATH_TO_UBERMOVEMENT_VAL = PATH_TO_UBERMOVEMENT + 'validation/'
     PATH_TO_UBERMOVEMENT_TEST = PATH_TO_UBERMOVEMENT + 'testing/'
+    
+    PATH_TO_CLIMART = PATH_TO_DATA + 'ClimART/'
+    PATH_TO_CLIMART_TRAIN = PATH_TO_CLIMART + 'training/'
+    PATH_TO_CLIMART_VAL = PATH_TO_CLIMART + 'validation/'
+    PATH_TO_CLIMART_TEST = PATH_TO_CLIMART + 'testing/'
+    
     
     
     ### Methods ###
