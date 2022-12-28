3dde98160f870a75aafbff4c89913e589eaaac31
Thomas Rigoni <thomas.rigoni7@gmail.com>
2022-12-28 20:32:41 +0000

    added deps and initial models file

---
+++ src/models.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+from torch_geometric.nn import GCNConv
+
+class VAE(nn.Module):
+    pass
+
+class GNN(nn.Module):
+    def __init__(self, in_channels: int, hidden_channels: int, out_channels: int):
+        super().__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, out_channels)
+
+    def forward(self, x: torch.Tensor, edge_index: torch.Tensor) -> torch.Tensor:
+        # x: Node feature matrix of shape [num_nodes, in_channels]
+        # edge_index: Graph connectivity matrix of shape [2, num_edges]
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index)
+        return x
+
+if __name__ == "__main__":
+    GNN(5, 10, 3)
