08d69e71a82e401ca22bf226c4a4fbdcbc6fc00e
A A <arsama@ethz.ch>
2023-01-08 13:39:17 +0100

    small tests with build and run

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,4 @@
 docker build Docker -t ubuntu-torch
-docker run -it --ipc=host --gpus 1 -v ~/UniversalGNNs/:/UniversalGNNs/ -v ~/TasksEnergyTransition/:/TasksEnergyTransition/ ubuntu-torch
+docker run -it --ipc=host --gpus 1 -v ~/Documents/projects/UniversalGNNs/:/UniversalGNNs/ -v ~/Documents/projects/TasksEnergyTransition/:/TasksEnergyTransition/ ubuntu-torch
+
 # docker run -it --gpus 1 -v ~/UniversalGNNs/:/UniversalGNNs/ -v /home/shared_cp/TasksEnergyTransition/:/TasksEnergyTransition/ ubuntu-torch
