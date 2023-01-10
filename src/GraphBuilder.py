f945198ba2ff3e616f477d0f389fcb83ead9f361
fdistefano99 <75171561+fdistefano99@users.noreply.github.com>
2023-01-10 17:17:53 +0100

    GPU distances computation added to graph builder

--- src/GraphBuilder.py
+++ src/GraphBuilder.py
@@ -4,7 +4,6 @@ import numpy as np
 import scipy
 
 class GraphBuilder:
-
     def __init__(self, distance_function, params_indeces, connectivity, encoder, edge_level_batch=False):
         self.distance_function = distance_function
         self.params_indeces = params_indeces
@@ -13,21 +12,26 @@ class GraphBuilder:
         self.edge_level_batch = edge_level_batch
     
     def compute_nodes_matrix(self, batch):
-        return self.encoder.get_latent(batch)
+        return batch
 
     def compute_edges_matrices(self, batch, device):
         distance_features_indeces = torch.IntTensor(self.params_indeces).to(device)
         distance_features = torch.index_select(batch, dim=1, index=distance_features_indeces)
-        distance_features = distance_features.cpu().detach().numpy()
-        distances_matrix = scipy.spatial.distance.cdist(distance_features, distance_features, self.distance_function)
+        if isinstance(self.distance_function, int):
+          print("ciao torch")
+          distances_matrix = torch.cdist(distance_features, distance_features, self.distance_function)
+        else:
+          print("ciao norm")
+          distance_features = distance_features.cpu().detach().numpy()
+          distances_matrix = scipy.spatial.distance.cdist(distance_features, distance_features, self.distance_function)
+          distances_matrix = torch.from_numpy(distances_matrix).to(device)
         min_distance, max_distance = distances_matrix.min(), distances_matrix.max()
         distances_matrix = (distances_matrix - min_distance) / (max_distance - min_distance)
-        score_function = np.vectorize(self.score_function)
-        scores_matrix = score_function(distances_matrix)
+        scores_matrix = 1 - distances_matrix
+        scores_matrix = scores_matrix - torch.eye(scores_matrix.shape[0]).to(device)
         sparsity = 1 - self.connectivity
-        quantile = np.quantile(scores_matrix, q=sparsity)
-        scores_matrix = np.where(scores_matrix > quantile, scores_matrix, 0.)
-        scores_matrix = torch.from_numpy(scores_matrix).to(device)
+        quantile = torch.quantile(scores_matrix, q=sparsity)
+        scores_matrix = torch.where(scores_matrix > quantile, scores_matrix, 0.)
         edges_indeces, edges_weights = torch_geometric.utils.dense_to_sparse(scores_matrix)
         return edges_indeces, edges_weights
 
