7f07ae0bf915878d1f2b1000ef9274b03406090a
Thomas Rigoni <thomas.rigoni7@gmail.com>
2023-01-13 10:59:13 +0000

    fixed log bug in MLP

--- src/models.py
+++ src/models.py
@@ -51,11 +51,11 @@ class MLP(pl.LightningModule):
         loss = F.mse_loss(out, y)
         r2 = r2_score(out, y)
         if len(batch) == 3:
-            self.log(f"{split} loss", loss, on_epoch=True, batch_size=len(x))
-            self.log(f"{split} R2", r2, on_epoch=True, batch_size=len(x))
-        elif len(batch) == 2:
             self.log(f"{dataset_name} {split} loss", loss, on_epoch=True, batch_size=len(x))
             self.log(f"{dataset_name} {split} R2", r2, on_epoch=True, batch_size=len(x))
+        elif len(batch) == 2:
+            self.log(f"{split} loss", loss, on_epoch=True, batch_size=len(x))
+            self.log(f"{split} R2", r2, on_epoch=True, batch_size=len(x))
         return loss
 
     def training_step(self, batch, batch_idx):
